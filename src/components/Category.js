f5bc13ece9d09bb7642c44a46aa144e7cc7ca891
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-09-23 15:35:07 +0300

    Create Category.js

---
+++ src/components/Category.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Author extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Loading ... </h2>
+        <button type="button">Check status</button>
+      </div>
+    );
+  }
+}
+
+export default Author;
