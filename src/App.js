1033e25bc3c81a4fdbd321e5ab68e13d50092722
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-09-23 15:35:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Books from './components/Books';
+import Category from './components/Category';
 
 function App() {
-  return null;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/Category" element={<Category />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
