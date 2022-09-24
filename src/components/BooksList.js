c75b12e0ef109eb1a298024a33ccb7091a07e95a
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-09-24 11:46:52 +0300

    Update BooksList.js

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,19 +1,26 @@
-const Bookslist = ({ books, handleDelete }) => {
-  return (
-    <div className="booksList">
-      {books.map((book) => (
-        <div className="prevBooks" key={book.id}>
-          <label>
-            {book.title}
-            {book.author}
-            {book.category}
-            <button type="button" onClick={() => handleDelete(book.id)}>
-              Remove button
-            </button>
-          </label>
-        </div>
-      ))}
-    </div>
-  );
+import PropTypes from 'prop-types';
+
+const Bookslist = ({ books, handleDelete }) => (
+  <div className="booksList">
+    {books.map((book) => (
+      <div className="prevBooks" key={book.id}>
+        {book.title}
+        {book.author}
+        {book.category}
+        <button type="button" onClick={() => handleDelete(book.id)}>
+          Remove button
+        </button>
+      </div>
+    ))}
+  </div>
+);
+
+Bookslist.defaultProps = {
+  books: '',
+};
+
+Bookslist.propTypes = {
+  books: PropTypes.string,
+  handleDelete: PropTypes.func.isRequired,
 };
 export default Bookslist;
