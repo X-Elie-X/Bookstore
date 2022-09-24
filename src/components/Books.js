2d7372fdbc4052d3fb5f2c03476390c9e1d45335
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-09-24 11:46:46 +0300

    Update Books.js

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,27 @@
 import { useState } from 'react';
 import Bookslist from './BooksList';
 import Diplaybooks from './Displaybooks';
+
 const Books = () => {
   const [books, setBooks] = useState([
-    { id: 1, title: 'books', author: 'books1', category: 'action' },
-    { id: 2, title: 'books2', author: 'books2', category: 'action1' },
-    { id: 3, title: 'books3', author: 'books3', category: 'action2' },
+    {
+      id: 1,
+      title: 'books',
+      author: 'books1',
+      category: 'action',
+    },
+    {
+      id: 2,
+      title: 'books2',
+      author: 'books2',
+      category: 'action1',
+    },
+    {
+      id: 3,
+      title: 'books3',
+      author: 'books3',
+      category: 'action2',
+    },
   ]);
   const handleDelete = (id) => {
     const newBooks = books.filter((book) => book.id !== id);
