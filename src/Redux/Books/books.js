c478257b7032df5fc016516312b8a05487468301
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-09-29 19:07:52 +0300

    Update books.js

--- src/Redux/Books/books.js
+++ src/Redux/Books/books.js
@@ -1,40 +1,54 @@
 export const ADD_BOOK = 'Books/books/ADD_BOOK';
 export const REMOVE_BOOK = 'Books/books/REMOVE_BOOK  ';
 
-export const initialState = [
-  {
-    id: 1,
-    title: 'hello world',
-    author: 'Hello',
-  },
-  {
-    id: 2,
-    title: 'hello Books',
-    author: 'book',
-  },
-];
+export const initialState = {
+  books: [
+    {
+      id: 1,
+      title: 'hello world',
+      author: 'Hello',
+      category: 'action',
+    },
+    {
+      id: 2,
+      title: 'hello Books',
+      author: 'book',
+      category: 'action',
+    },
+  ],
+};
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
-      return [...state, action.books];
+      return {
+        ...state,
+
+        books: [...state.books, action.payload],
+      };
 
     case REMOVE_BOOK:
-      return state.filter((books) => books !== action.booksId);
-
+      return {
+        ...state,
+        books: state.books.filter((book) => book.id !== action.payload),
+      };
     default:
       return state;
   }
 };
 
-export const AddBook = (books) => ({
-  type: 'ADD_BOOK',
-  books,
-});
+export const AddBook = (values) => (dispatch) => {
+  dispatch({
+    type: ADD_BOOK,
+    payload: values,
+  });
+};
 
-export const RemoveBook = (booksId) => ({
-  type: 'REMOVE_BOOK',
-  booksId,
-});
+export const RemoveBook = (id) => (dispatch) => {
+  dispatch({
+    type: REMOVE_BOOK,
+    payload: id,
+  });
+};
 
 export default bookReducer;
