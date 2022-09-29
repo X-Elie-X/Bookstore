df4bcbbc3c739071ae0ff3257b3b0eb945c67f70
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-09-29 19:08:12 +0300

    Update BooksList.js

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,27 +1,5 @@
 import Book from './Book';
 
-const Bookslist = () => {
-  const books = [
-    {
-      id: 1,
-      title: 'books',
-      author: 'books1',
-      category: 'action',
-    },
-    {
-      id: 2,
-      title: 'books2',
-      author: 'books2',
-      category: 'action1',
-    },
-    {
-      id: 3,
-      title: 'books3',
-      author: 'books3',
-      category: 'action2',
-    },
-  ];
-  return <Book books={books} />;
-};
+const Bookslist = () => <Book />;
 
 export default Bookslist;
